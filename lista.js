40c903b886e4151699059f25e918892702f253d9
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2024-01-03 19:41:53 -0300

    commit inicial

---
+++ lista.js
@@ -0,0 +1,87 @@
+const URL = "https://crudcrud.com/api/b3a99f534bcf4994996eb9c585a9c3da/personagens";
+
+async function getPersonagem() {
+  const response = await fetch(URL);
+  return await response.json();
+}
+
+async function deletePersonagem(id) {
+  const response = await fetch(URL + `/${id}`, { method: "DELETE" });
+  if (response.status == 200) return "Você excluiu o personagem";
+  return "Algo deu errado";
+}
+
+function createPersonagem(personagem, index) {
+  const container = document.querySelector(".container");
+  const id = `personagem-${personagem._id}`;
+
+  let elem = document.querySelector(`div#${id}`);
+  if (elem) return;
+
+  elem = document.createElement("div");
+  elem.id = id;
+  elem.className = "elem";
+
+  const img = document.createElement("img");
+  img.src = `images/perfil${personagem.genero}.jpg`;
+  img.width = "50";
+  img.className = "imgElem";
+
+  const nome = document.createElement("a");
+  nome.innerText = `Nome: ${personagem.nome}`;
+  nome.className = "nome";
+  nome.href = `./registro.html?id=${personagem._id}`;
+
+  const tend = document.createElement("p");
+  tend.className = "tend";
+  tend.innerText = `Tendência: ${personagem.tendencia}`;
+
+  const gen = document.createElement("p");
+  gen.className = "generoLista";
+  gen.innerText = `Gênero: ${personagem.genero}`;
+
+  const nota = document.createElement("p");
+  nota.className = "nota";
+  nota.innerText = `Nota: ${personagem.nota}`;
+
+  const btnDelete = document.createElement("button");
+  btnDelete.className = "btnDelete botao"
+  btnDelete.innerText = "Remover";
+  btnDelete.id = "delete";
+
+  btnDelete.addEventListener("click", () => {
+    if (confirm("Você tem certeza que quer remover este personagem da lista????????")) {
+      elem.remove();
+      deletePersonagem(personagem._id).then((msg) => alert(msg));
+    }
+  });
+
+  const btnVer = document.createElement("button");
+  btnVer.className = "btnVer botao"
+  btnVer.innerText = "Ver Mais";
+  btnVer.id = "verMais";
+
+  btnVer.addEventListener("click", () => {
+    alert(personagem.descricao);
+  });
+
+  elem.appendChild(img);
+  elem.appendChild(nome);
+  elem.appendChild(tend);
+  elem.appendChild(gen);
+  elem.appendChild(nota);
+  elem.appendChild(btnDelete);
+  elem.appendChild(btnVer);
+
+  container.appendChild(elem);
+}
+
+const interval = setInterval(() => {
+  getPersonagem().then((listaPersonagens) => {
+    listaPersonagens.forEach(createPersonagem);
+  });
+}, 5000);
+
+setTimeout(() => {
+  clearInterval(interval);
+}, 5000);
---
+++ registro.js
@@ -0,0 +1,112 @@
+const URL = "https://crudcrud.com/api/b3a99f534bcf4994996eb9c585a9c3da/personagens";
+console.log("entrou no js do registro");
+// import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+const generos = document.getElementsByClassName("genero");
+
+async function getPersonagem(id) {
+  if (id) {
+    const response = await fetch(URL + `/${id}`);
+    const personagem = await response.json();
+
+    nome.value = personagem.nome;
+    tendencia.value = personagem.tendencia;
+    nota.value = personagem.nota;
+    descricao.value = personagem.descricao;
+
+    for (let i = 0; i < generos.length; i++) {
+      if (generos[i].value == personagem.genero) {
+        generos[i].checked = true;
+      }
+    }
+  }
+  console.log(personagem);
+}
+
+async function addPersonagem(objPersonagem) {
+  const response = await fetch(URL, {
+    method: "POST",
+    body: JSON.stringify(objPersonagem),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  return await response.json();
+}
+
+async function updatePersonagem(id, objPersonagem) {
+  console.log("editou");
+  const response = await fetch(URL + `/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(objPersonagem),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  return await response.json();
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+getPersonagem(id);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    nome,
+    tendencia,
+    genero,
+    nota,
+    descricao
+  } = event.target;
+
+  const novoPersonagem = {
+    nome: nome.value,
+    tendencia: tendencia.value,
+    genero: genero.value,
+    nota: nota.value,
+    descricao: descricao.value
+  };
+
+  //colocar validaçoes
+  nomeError.innerText = "";
+  tendenciaError.innerText = "";
+  generoError.innerText = "";
+  notaError.innerText = "";
+  descError.innerText = "";
+
+  if (novoPersonagem.nome == "") {
+    nomeError.innerText = "Nome é obrigatório";
+    return;
+  }
+
+  if (novoPersonagem.tendencia == "") {
+    tendenciaError.innerText = "Tendência é obrigatória";
+    return;
+  }
+
+  if (novoPersonagem.nota == "") {
+    notaError.innerText = "Nota é obrigatória";
+    return;
+  } else if (parseFloat(novoPersonagem.nota) <= 0) {
+    notaError.innerText = "Nota deve ser um valor acima de zero";
+    return;
+  }
+
+  if (novoPersonagem.genero == "") {
+    generoError.innerText = "Gênero é obrigatório";
+    return;
+  }
+
+  if (novoPersonagem.descricao == "") {
+    imgError.innerText = "Descrição é obrigatória";
+    return;
+  }
+
+  if (id) {
+    updatePersonagem(id, novoPersonagem).then(console.log);
+  } else {
+    addPersonagem(novoPersonagem);
+  }
+
+  form.reset();
+  
+  window.location = './index.html';
+});
